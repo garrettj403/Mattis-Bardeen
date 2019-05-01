28e3881e3049b5731b9f316dd2c7a84102641de3
John Garrett <john.garrett@astro.ox.ac.uk>
2019-05-01 08:27:30 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Mattis-Bardeen",
+    version = "0.0.1dev",
+    author = "John Garrett",
+    author_email = "garrettj403@gmail.com",
+    description = ("Calculate the electrical properties of superconductors using Mattis-Bardeen theory."),
+    license = "GPL v3",
+    keywords = "superconductivity, physics, superconducting detectors, terahertz instrumentation, Python",
+    # url = "https://garrettj403.github.io/",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    long_description="Calculate the electrical properties of superconductors using Mattis-Bardeen theory.",
+    classifiers=[
+        # "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Physics",
+        # "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+)
